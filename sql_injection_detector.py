86faa48a86da841ee0f03985b75714b1246dfae9
Lazar Nikitovic <lazonikitovic@gmail.com>
2023-08-16 23:07:31 +0200

    Update sql_injection_detector.py

--- sql_injection_detector.py
+++ sql_injection_detector.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 
-
 class sql_injection_detector:
     s = requests.Session()
     s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
